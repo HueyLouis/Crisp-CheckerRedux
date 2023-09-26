7495eeb1ff38edcf366f3a7f2f6a4e52512b315c
Huey Louis <140862578+HueyLouis@users.noreply.github.com>
2023-09-26 14:33:35 -0400

    Finished contentScript.js, DEBUG needed

--- js/contentScript.js
+++ js/contentScript.js
@@ -87,4 +87,95 @@ document.getElementById('copyButton').addEventListener('click', function () {
     Display the results in the div element 
     ****************************************/
    
+    const resultsDiv = document.createElement('div');
+    resultsDiv.style.cssText = `
+        position: fixed;
+        top: 10px;
+        right: 10px;
+        background-color: #ffffff;
+        border: 1px solid #ccc;
+        border-radius 10px;
+        padding: 20px;
+        box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.1);
+        z-index: 1000;
+        font-family: Ariel, sans-serif;
+        font-size: 0.9em; // Adjust this value to change the font size
+        width: 300px; // Set a fixed width for the div
+        overflow: auto; // Add a scrollbar if the content too long
+    `;
+
+    // Create a header div for dragging
+    const headerDiv = document.createElement('div');
+    headerDiv.style.cssText = `
+        height: 20px;
+        background-color: #ddd;
+        cursor: move;
+        width: 100%;
+        text-align: center;
+        border-bottom: 1px solid #ccc
+    `;
+    headerDiv.textContent = "Drag from here";
+
+    // Append the header div to the results div
+    resultsDiv.appendChild(headerDiv);
+
+    // Variable for dragging
+    let dragging = false;
+    let offsetX, offsetY;
+
+    // Function to handle mouse down event 
+    function onMouseDown(e) {
+        const rect = resultsDiv.getBoundingClientRect();
+        if (e.clientY - rect.top <= 20) { // Check if the mouse is within the top 20 pixels
+            dragging = true;
+            offsetX = e.clientX - rect.left;
+            offsetY = e.clientY - rect.top;
+        }
+    }
+
+    // Function handle mouse up event
+    function onMouseUp() {
+        dragging = false;
+    }
+
+    // Function to handle mouse move event
+    function onMouseMove(e) {
+        if (dragging) {
+            resultsDiv.style.left = (e.clientX - offsetX) + 'px';
+            resultsDiv.style.top = (e.clientY - offsetY) + 'px';
+        }
+    }
+
+    // Add event listners for dragging
+    headerDiv.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mousemove', onMouseMove);
+
+    //Create close button
+    const closeButton = document.createElement('button');
+    closeButton.innerHTML = 'Close';
+    closeButton.style.cssText = `
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        background-color: #ddd;
+        border: none;
+        border-radius: 5px;
+        padding: 5pxl
+        cursor: pointer;
+    `;
+
+    // Function to handle the close button click event
+    function onCloseButtonClick() {
+        document.body.removeChild(resultsDiv);
+    }
+
+    // add event listener for the close button click
+    closeButton/addEventListener('click', onCloseButtonClick);
+
+    // Append the close button to the header div
+    headerDiv.appendChild(closeButton);
+
+    // Append the results div to the page
+    document.body.appendChild(resultsDiv);
 });
